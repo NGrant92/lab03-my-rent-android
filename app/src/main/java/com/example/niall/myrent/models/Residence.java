1bd0ca1bb99da356610dc43069478538491c22ca
NGrant <niall.grant1992@gmail.com>
2017-09-27 22:01:45 +0100

    Added added date field to Residence.java

--- app/src/main/java/com/example/niall/myrent/models/Residence.java
+++ app/src/main/java/com/example/niall/myrent/models/Residence.java
@@ -1,16 +1,21 @@
 package com.example.niall.myrent.models;
 
+import java.util.Date;
 import java.util.Random;
 
-public class Residence {
-  private Long id;
+public class Residence
+{
+  public Long id;
+  public Long date;
 
   //a latitude longitude pair
   //example "52.4566,-6.5444"
   private String geolocation;
+  public boolean rented;
 
   public Residence() {
     id = unsignedLong();
+    date = new Date().getTime();
   }
 
   /**
@@ -33,4 +38,14 @@ public class Residence {
   public String getGeolocation() {
     return geolocation;
   }
+
+  public String getDateString() {
+    return "Registered:" + dateString();
+  }
+
+  private String dateString() {
+    String dateFormat = "EEE d MMM yyyy H:mm";
+    return android.text.format.DateFormat.format(dateFormat, date).toString();
+  }
+
 }
