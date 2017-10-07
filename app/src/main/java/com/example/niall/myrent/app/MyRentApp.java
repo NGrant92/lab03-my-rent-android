6aa5a8b96bcc99dfbcd243b7c192c12176ce875b
NGrant <niall.grant1992@gmail.com>
2017-10-07 22:24:16 +0100

    MyRentApp.java implements PortfolioSerializer

--- app/src/main/java/com/example/niall/myrent/app/MyRentApp.java
+++ app/src/main/java/com/example/niall/myrent/app/MyRentApp.java
@@ -2,17 +2,21 @@ package com.example.niall.myrent.app;
 
 import android.app.Application;
 import com.example.niall.myrent.models.Portfolio;
+import com.example.niall.myrent.models.PortfolioSerializer;
+
 import static com.example.niall.myrent.android.helpers.LogHelpers.info;
 
 public class MyRentApp extends Application
 {
   public Portfolio portfolio;
+  private static final String FILENAME = "portfolio.json";
 
   @Override
   public void onCreate()
   {
     super.onCreate();
-    portfolio = new Portfolio();
+    PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
+    portfolio = new Portfolio(serializer);
 
     info(this, "MyRent app launched");
   }
